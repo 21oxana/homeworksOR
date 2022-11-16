9196586c608f709431346521f3eb1a830ee73f2a
Oksana R <oksanarybkinaa@gmail.com>
2022-11-16 21:46:55 +0300

    do the ex.4

---
+++ EX_4/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 4: Напишите программу, которая принимает на вход три числа 
+и выдаёт максимальное из этих чисел.*/
+
+Console.WriteLine("Введите первое числа");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе числа");
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье числа");
+int numberThird = Convert.ToInt32(Console.ReadLine());
+
+if (numberFirst > numberSecond)
+{
+    Console.WriteLine($"наибольшее число {numberFirst}");
+}
+if (numberSecond > numberThird)
+{
+    Console.WriteLine($"наибольшее число {numberSecond}");
+}
+if (numberThird > numberFirst)
+{
+    Console.WriteLine($"наибольшее число {numberThird}");
+}
