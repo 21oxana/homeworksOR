81c99b9f2a604a26b61b182d1c6e79b11eaffd84
Oksana R <oksanarybkinaa@gmail.com>
2022-11-16 22:47:15 +0300

    do the ex.8

---
+++ EX_8/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N > 0),
+ а на выходе показывает все чётные числа от 1 до N. */
+
+Console.WriteLine("Введите число больше 0");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = 2;
+
+if(number > 1)
+{
+    while(count <= number)
+    {
+        Console.Write(count + " ");
+        count = count + 2;
+    }
+}
+Console.WriteLine();
+
+
